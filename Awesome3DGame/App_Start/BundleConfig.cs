07017572dafe85c6bfd30e5704cc810b50401dac
SneakyBrian <SneakyBrian@gmail.com>
2013-04-17 22:56:40 +0100

    Added camera controls

--- Awesome3DGame/App_Start/BundleConfig.cs
+++ Awesome3DGame/App_Start/BundleConfig.cs
@@ -19,7 +19,8 @@ namespace Awesome3DGame
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/three").Include(
-                        "~/Scripts/three*"));
+                        "~/Scripts/three*",
+                        "~/Scripts/FlyControls.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
